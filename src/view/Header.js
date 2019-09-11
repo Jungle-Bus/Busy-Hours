025689d07f0df3d37f39855e8cd3f1a23f8567d1
PanierAvide <panieravide@riseup.net>
2019-09-11 16:35:49 +0200

    Integrate logo in header

--- src/view/Header.js
+++ src/view/Header.js
@@ -9,12 +9,21 @@ import UserButton from './UserButton';
  */
 class Header extends Component {
 	render() {
-		return <AppBar position="static">
+		return <AppBar position="static" style={{backgroundColor: "#0267b1"}}>
 			<Toolbar style={{justifyContent: "space-between"}}>
-				<Typography variant="h6">
-					{window.EDITOR_NAME}
-				</Typography>
-				
+				<div>
+					<img
+						src="header.png"
+						height={64}
+						title={window.EDITOR_NAME}
+						alt=""
+						style={{verticalAlign: "middle", marginRight: 10}}
+					/>
+					<Typography variant="h6" style={{display: "inline-block"}}>
+						{window.EDITOR_NAME}
+					</Typography>
+				</div>
+
 				<UserButton />
 			</Toolbar>
 		</AppBar>;
